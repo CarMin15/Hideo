a72b7b6970407df28d468788a488e26629b2e4f3
María Rosa <CarMin15@users.noreply.github.com>
2016-01-16 23:04:07 -0800

    map update

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,12 @@
-
-
+function initialize() {
+		  var mapProp = {
+		    center:new google.maps.LatLng(51.508742,-0.120850),
+		    zoom:5,
+		    mapTypeId:google.maps.MapTypeId.ROADMAP
+		  };
+		  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
 
 $(document).ready(function() {
 	var menu_sticky_top = $('.menu_sticky').position().top;
