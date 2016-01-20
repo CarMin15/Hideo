5df656fedf8848879b27209b49222d2cf7a505a5
María Rosa <CarMin15@users.noreply.github.com>
2016-01-19 17:57:14 -0800

    header and nav bar small changes

--- JS/main.js
+++ JS/main.js
@@ -16,10 +16,8 @@ $(document).ready(function() {
 
 		if (document.body.scrollTop > menu_sticky_top) {
 			$menu.addClass('sticky');
-			$menu.next().css('margin-top', $('.menu_sticky').height());
 		} else {
 			$menu.removeClass('sticky');
-			$menu.next().css('margin-top', 0);
 		}
 	});
 });
